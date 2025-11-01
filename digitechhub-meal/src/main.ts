edd8be81aa7e07fce4c784a8e86f235fadae49f6
balhyo-younjisang <yjs12180825@gmail.com>
2025-11-02 01:23:15 +0900

    feat(swagger): Meal 서비스 Swagger 설정 추가

--- digitechhub-meal/src/main.ts
+++ digitechhub-meal/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Digitech Hub Meal API')
+    .setDescription('급식 정보 제공을 위한 마이크로서비스 API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
